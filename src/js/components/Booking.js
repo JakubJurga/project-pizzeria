06383ee084c036100356d79f86912e2d9bb7d9dd
Jakub Jurga <>
2021-04-28 13:56:21 +0100

    Add getData

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,7 +1,8 @@
-import {select, templates} from '../settings.js';
-import AmountWidget from './AmountWidget.js';
-import DatePicker from './DatePicker.js';
-import HourPicker from './HourPicker.js';
+import {select, templates, settings, classNames} from '../settings.js';
+import utils from '../utils.js';
+import AmountWidget from '../components/AmountWidget.js';
+import DatePicker from '../components/DatePicker.js';
+import HourPicker from '../components/HourPicker.js';
 
 class Booking {
   constructor(element){
@@ -9,7 +10,58 @@ class Booking {
 
     thisBooking.render(element);
     thisBooking.initWidgets();
+    thisBooking.getData();
 
+  }
+
+  getData() {
+    const thisBooking = this;
+    const startDateParam = settings.db.dateStartParamKey + '=' + utils.dateToStr(thisBooking.datePicker.minDate);
+    const endDateParam = settings.db.dateEndParamKey + '=' + utils.dateToStr(thisBooking.datePicker.maxDate);
+
+    const params = {
+       booking: [
+         startDateParam,
+         endDateParam,
+       ],
+       eventsCurrent: [
+         settings.db.notRepeatParam,
+         startDateParam,
+         endDateParam,
+       ],
+       eventsRepeat: [
+         settings.db.repeatParam,
+         endDateParam,
+       ],
+     };
+
+    const urls = {
+    booking: settings.db.url + '/' + settings.db.booking     + '?' + params.booking.join('&'),
+    eventsCurrent: settings.db.url + '/' + settings.db.event + '?' + params.eventsCurrent.join('&'),
+    eventsRepeat: settings.db.url + '/' + settings.db.event  + '?' + params.eventsRepeat.join('&'),
+  };
+
+    Promise.all([
+      fetch(urls.booking),
+      fetch(urls.eventsCurrent),
+      fetch(urls.eventsRepeat),
+    ])
+      .then(function(allResponses){
+        const bookingsResponse = allResponses[0];
+        const eventsCurrentResponse = allResponses[1];
+        const eventsRepeatResponse = allResponses[2];
+        return Promise.all([
+          bookingsResponse.json(),
+          eventsCurrentResponse.json(),
+          eventsRepeatResponse.json(),
+        ]);
+      })
+      .then(function([bookings, eventsCurrent, eventsRepeat]){
+        console.log(bookings);
+        console.log(eventsCurrent);
+        console.log(eventsRepeat);
+
+      });
   }
 
   render(element) {
@@ -41,6 +93,7 @@ class Booking {
 
     thisBooking.peopleAmountWidget = new AmountWidget (thisBooking.dom.peopleAmount);
     thisBooking.dom.peopleAmount.addEventListener ('click', function(){});
+
 
     thisBooking.hoursAmountWidget = new AmountWidget (thisBooking.dom.hoursAmount);
     thisBooking.dom.hoursAmount.addEventListener ('click', function(){});
